77bf46e12b0da57d66b66dd2f0e738343526e5a1
k <j>
2024-10-03 17:21:36 -0600

    Added data_processing and pitch map

---
+++ src/data_processing/pitch_mappings.py
@@ -0,0 +1,4 @@
+
+
+PITCH_MAP = {
+    'CH': 'OS', 'CS': 'BB', 'CU': 'BB', 'EP': 'OS', 'FA': '?', 'FC': 'FB', 'FF': 'FB', 'FO': 'FB', 'FS': 'FB', 'KC': 'BB', 'KN': 'BB', 'PO': '?', 'SC': 'BB', 'SI': 'FB', 'SL': 'BB', 'ST': 'BB', 'SV': 'BB'}
